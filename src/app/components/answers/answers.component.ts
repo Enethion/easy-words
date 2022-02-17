b863a2da950dbe3ce840ab9c80c11445926e2074
Piotr <straszak.piotr@gmail.com>
2022-02-17 14:15:08 +0100

    Section 30: Services

--- src/app/components/answers/answers.component.ts
+++ src/app/components/answers/answers.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WordType } from 'src/app/data/models';
 
 @Component({
   selector: 'app-answers',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./answers.component.css']
 })
 export class AnswersComponent implements OnInit {
+  
+  @Input() words: WordType[]
 
   constructor() { }
 
